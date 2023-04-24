d3fd48340fffa41308208167d5cde871a778d41c
Elijah <elijahferreria18@gmail.com>
2023-04-24 20:07:50 +0800

    just removed spaces

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ function getComputerChoice(){
     let rps = ['rock', 'paper', 'scissor'];
     
     let randNum = Math.floor(Math.random() * rps.length);
-    
     let choice = rps[randNum];
-    
     console.log(choice);
 }
 

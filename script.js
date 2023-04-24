afa2e32129296f6fc0b79e2cc799e2357bde2e29
Elijah <elijahferreria18@gmail.com>
2023-04-24 20:20:17 +0800

    added playerSelection function for player input between rock paper and scissor

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 
-
-
-function getComputerChoice(){
-    let rps = ['rock', 'paper', 'scissor'];
-    
-    let randNum = Math.floor(Math.random() * rps.length);
-    let choice = rps[randNum];
-    console.log(choice);
+function playerSelection(){
+    let playerChoice = prompt("Pick between rock, paper, and scissors");
+    playerChoice.toLowerCase();
+    console.log(playerChoice);
 }
 
+// A function for the computer to pick betweek rock paper scissor
+function getComputerChoice(){
+    let rps = ['rock', 'paper', 'scissor']; 
+    let randNum = Math.floor(Math.random() * rps.length); 
+    let computerChoice = rps[randNum];
+    console.log(computerChoice);
+}
+
+playerSelection();
 getComputerChoice();
+

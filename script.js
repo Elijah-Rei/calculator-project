9ee1def9038e1d82a61ed820655f01c0fe4966bb
Elijah <elijahferreria18@gmail.com>
2023-05-10 17:08:45 +0800

    added 3 buttons for rock paper scissors

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
 // A function to ask the user between rock paper and scissor
+/*
 function playerSelection(){
-    let playerChoice = prompt("Pick between rock, paper, and scissors");
+    let playerChoice = 'rock'; //prompt("Pick between rock, paper, and scissors");
     playerChoice.toLowerCase();
     return playerChoice;
-}
+}*/
 
 // A function for the computer to pick between rock paper scissor
+const btn1 = document.querySelector('#btnRck');
+    btn1.addEventListener('click', () => {
+    playRound('rock', getComputerChoice());
+});
+
+const btn2 = document.querySelector('#btnPpr');
+    btn2.addEventListener('click', () => {
+    playRound('paper', getComputerChoice());
+});
+
+const btn3 = document.querySelector('#btnSci');
+    btn3.addEventListener('click', () => {
+    playRound('scissors', getComputerChoice());
+});
+
 function getComputerChoice(){
     let rps = ['rock', 'paper', 'scissor']; 
-    let randNum = Math.floor(Math.random() * rps.length); 
+    let randNum = Math.floor(Math.random() * rps.length);
     let computerChoice = rps[randNum];
     return computerChoice;
 }
 
 function playRound(playerPick, computerPick){
     console.log("You chose: " + playerPick + " and Opponent chose: " + computerPick);
+    game(playerPick, computerPick);
 }
 
-function game(){ // game() function that calls the playerSelection() and getComputerChoice() and loops it 5 times
+function game(playerPick, computerPick){ // game() function that calls the playerSelection() and getComputerChoice() and loops it 5 times
     let i = 0;
     let playerScore = 0;
     let computerScore = 0;
+
     while (i < 5){
-        let playerPick = playerSelection();
-        let computerPick = getComputerChoice();
-        playRound(playerPick, computerPick);
-        
         if (playerPick == computerPick){
             console.log("Tie");
         }else if(playerPick == 'rock' && computerPick == 'scissor' || playerPick == 'paper' && computerPick == 'rock' || playerPick == 'scissor' && computerPick == 'paper'){
@@ -35,7 +49,6 @@ function game(){ // game() function that calls the playerSelection() and getComp
             console.log("You lost!");
             computerScore++;
         }
-
         i++
     }
 
@@ -47,4 +60,4 @@ function game(){ // game() function that calls the playerSelection() and getComp
         console.log("Score is " + playerScore + " - " + computerScore+ " its a tie!")
 }
 
-game();
+//game();
